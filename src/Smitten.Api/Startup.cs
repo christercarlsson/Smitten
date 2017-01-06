248c0d81c8f7af0a413f07f8eae24643ef70d36a
Christer Carlsson <ccarlsson@outlook.com>
2017-01-06 13:59:47 +0100

    Add Configuration

--- src/Smitten.Api/Startup.cs
+++ src/Smitten.Api/Startup.cs
@@ -9,36 +9,45 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Smitten.Api.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Smitten.Api
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IHostingEnvironment env) {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appSettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             services.AddMvc();
 
-            var connectionString = @"Server=(localdb)\mssqllocaldb;Database=SmittenDb;Trusted_Connection=True";
+            var connectionString = Configuration["connectionStrings:smittenDbConnectionString"];
             services.AddDbContext<SmittenContext>(o => o.UseSqlServer(connectionString));
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
             loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseMvc();
 
-            app.Run(async (context) =>
-            {
+            app.Run(async (context) => {
                 await context.Response.WriteAsync("Hello World!");
             });
         }
